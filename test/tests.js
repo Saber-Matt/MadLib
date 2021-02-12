fc144bc56ab8574971c6a3c4b727ad9fcf87e5b8
Matthew Saber <mattsaber21@yahoo.com>
2021-02-12 15:32:23 -0800

    javascript additions

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,15 @@
 import './example.test.js';
+
+test('sync input to element', (expect) => {
+    //Arrange
+    const input = document.createElement('input');
+    const span = document.createElement('span');
+    const expected = 'banana';
+    input.value = expected;
+    
+    //Act 
+    sync(input, span);
+
+    //Expect
+    expect.equal(span.textContent, expected);
+});
